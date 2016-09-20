556d93860d8d1abf572f12e763e5bfd1e55731c8
Rocky <irockytan@gmail.com>
2016-09-20 16:38:41 +0800

    添加 gateway.properties 配置文件

--- user-service/src/main/java/com/example/UserServiceApplication.java
+++ user-service/src/main/java/com/example/UserServiceApplication.java
@@ -1,16 +1,21 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.integration.annotation.MessageEndpoint;
+import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +26,7 @@ import javax.persistence.Id;
 import java.util.Collection;
 import java.util.stream.Stream;
 
+@EnableBinding(Sink.class)
 @SpringBootApplication
 @EnableDiscoveryClient
 public class UserServiceApplication {
@@ -37,6 +43,20 @@ public class UserServiceApplication {
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
 }
+
+@MessageEndpoint
+class UserMessageReceiver{
+
+	@Autowired
+	UserRepository userRepository;
+
+	@ServiceActivator(inputChannel = Sink.INPUT)
+	public void receive(String name){
+		userRepository.save(new User(name));
+	}
+
+}
+
 
 @RefreshScope
 @RestController
